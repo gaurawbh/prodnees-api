7e8d6de1952a4916972ff624e94b86515c5dae80
g09.shah <g09.shah@gmail.com>
2021-03-17 00:23:31 +0000

    LoginUserDetailsService

--- src/main/java/com/prodnees/service/LoginUserDetailsService.java
+++ src/main/java/com/prodnees/service/LoginUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.prodnees.service;
 
+import com.prodnees.dao.UserDao;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +9,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LoginUserDetailsService implements UserDetailsService {
+    private final UserDao userDao;
+
+    public LoginUserDetailsService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        com.prodnees.domain.User user = userDao.findByEmail(userName);
+
+        if (user.getEmail().equals(userName)) {
+            return User.withUsername(user.getEmail())
+                    .password(user.getPassword())
+                    .roles(user.getRole())
+                    .disabled(!user.isEnabled()).build();
+        } else {
+            throw new UsernameNotFoundException("user not found with username : ${username}");
+        }
     }
 }
